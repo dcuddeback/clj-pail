03684495894693894c6ceb9464bc6845a13d5583
David Cuddeback <david.cuddeback@gmail.com>
2013-05-28 16:00:33 -0700

    starts a new leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pail
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject clj-pail "0.1.0-SNAPSHOT"
+  :description "A Clojure abstraction for Pail"
+  :url "https://github.com/dcuddeback/clj-pail"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :min-lein-version "2.0"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]
+                        ["snapshots" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]]
+
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.1"]]}})

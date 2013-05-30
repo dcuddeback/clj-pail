999be66913bdda7faed5ecc44f5b3eac83b55c10
David Cuddeback <david.cuddeback@gmail.com>
2013-05-30 13:57:26 -0700

    configures environments for different Clojure versions

--- project.clj
+++ project.clj
@@ -14,7 +14,20 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]
                         ["snapshots" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]]
 
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.0.1"]]
                    :source-paths ["src/test"]
-                   :aot [clj-pail.fakes.structure]}})
+                   :aot [clj-pail.fakes.structure]}}
+
+  :aliases {"all" ["with-profile" "+1.3:+1.4:+1.5:+1.6"]}
+
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})

5cc887c0f440a7a9a1ca66e1ca078bb917482cff
David Cuddeback <david.cuddeback@gmail.com>
2013-06-03 17:56:31 -0700

    fixes test failures in Clojure 1.3

--- test/clj_pail/core_test.clj
+++ test/clj_pail/core_test.clj
@@ -1,6 +1,6 @@
 (ns clj-pail.core-test
-  (:require [clj-pail.core :as pail]
-            [midje.sweet :refer :all])
+  (:require [clj-pail.core :as pail])
+  (:use midje.sweet)
   (:import (com.backtype.hadoop.pail PailSpec PailStructure)))
 
 
@@ -13,5 +13,5 @@
 (facts "spec"
   (let [structure (proxy [PailStructure] [])]
     (fact "creates a PailSpec from a PailStructure"
-      (pail/spec structure) => (instance-of PailSpec)
+      (pail/spec structure) => (instance-of PailSpec )
       (.getStructure (pail/spec structure)) => structure)))

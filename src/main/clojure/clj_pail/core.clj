f8a33fa78bcb50b8bb78ed9a5ef09b4ddcc0c66a
David Cuddeback <david.cuddeback@gmail.com>
2013-07-01 16:41:50 -0700

    adds macro for working with Pail snapshots

--- src/main/clojure/clj_pail/core.clj
+++ src/main/clojure/clj_pail/core.clj
@@ -23,3 +23,38 @@
   (if filesystem
     (Pail/create filesystem path spec fail-on-exists)
     (Pail/create path spec fail-on-exists)))
+
+
+(defmacro with-snapshot
+  "Automatically deletes Pail snapshots after successfuly executing a block of code. The snapshots
+  should be created with `Pail.snapshot()` and will only be deleted if the body finishes
+  successfully. If the body throws an exception, the snapshot will not be deleted.
+
+  This is intended to be used to safely delete data that was successfully processed while not
+  deleting data that fails to process.
+
+  The first argument should be the original Pail that the snapshots are derived from. If the body
+  finishes correctly, this is the Pail from which the data will be deleted.
+
+  The second argument is a vector of bindings. The bindings should specify snapshots that can be
+  deleted from the original Pail with `Pail.deleteSnapshot()`.
+
+  Example:
+
+    (with-snapshot original-pail [snapshot-pail (.snapshot origin-pail \"/path/to/snapshot\")]
+      ...)"
+  [pail bindings & body]
+  (cond
+    (empty? bindings)
+    `(do ~@body)
+
+    (symbol? (first bindings))
+    (let [[this-binding rest-bindings] (split-at 2 bindings)
+          snapshot (first this-binding)]
+      `(let ~(vec this-binding)
+         (let [result# (with-snapshot ~pail ~rest-bindings ~@body)]
+           (.deleteSnapshot ~pail ~snapshot)
+           result#)))
+
+    :else
+    (throw (IllegalArgumentException. "with-snapshot only allows symbols in bindings"))))
--- test/clj_pail/core_test.clj
+++ test/clj_pail/core_test.clj
@@ -1,7 +1,10 @@
 (ns clj-pail.core-test
-  (:require [clj-pail.core :as pail])
+  (:require [clj-pail.core :as pail]
+            [clojure.java.shell :as shell])
   (:use midje.sweet)
-  (:import (com.backtype.hadoop.pail PailSpec PailStructure)))
+  (:import (com.backtype.hadoop.pail PailSpec PailStructure DefaultPailStructure)
+           (org.apache.hadoop.fs FileSystem Path)
+           (org.apache.hadoop.conf Configuration)))
 
 
 (defchecker instance-of
@@ -9,9 +12,60 @@
   (checker [actual]
     (instance? expected actual)))
 
+(defchecker array-eq
+  [expected]
+  (checker [actual]
+           (java.util.Arrays/equals expected actual)))
+
 
 (facts "spec"
   (let [structure (proxy [PailStructure] [])]
     (fact "creates a PailSpec from a PailStructure"
       (pail/spec structure) => (instance-of PailSpec )
       (.getStructure (pail/spec structure)) => structure)))
+
+
+(let [tmp-path "tmp/test"]
+  (with-state-changes [(before :contents (shell/sh "rm" "-rf" tmp-path))
+                       (after :contents (shell/sh "rm" "-rf" tmp-path))]
+
+    (facts "with-snapshot"
+      (let [pail-path (str tmp-path "/pail")
+            snap-path (str tmp-path "/snapshot")
+            pail (-> (DefaultPailStructure.)
+                   (pail/spec)
+                   (pail/create pail-path :fail-on-exists false))
+
+            before-1 (byte-array 0)
+            before-2 (byte-array (map byte (range 10)))
+            during-1 (byte-array 8)
+            during-2 (byte-array (map byte (range 8)))]
+
+        (with-state-changes [(before :contents (with-open [writer (.openWrite pail)]
+                                              (doto writer
+                                                (.writeObject before-1)
+                                                (.writeObject before-2))))]
+
+          (let [snapshot-objects (pail/with-snapshot pail [snapshot (.snapshot pail snap-path)]
+                                   (with-open [writer (.openWrite pail)]
+                                     (doto writer
+                                       (.writeObject during-1)
+                                       (.writeObject during-2)))
+                                   (iterator-seq (.iterator snapshot)))
+                pail-objects (iterator-seq (.iterator pail))]
+
+            (fact "snapshot sees data written before snapshot"
+              snapshot-objects => (contains (array-eq before-1))
+              snapshot-objects => (contains (array-eq before-2)))
+
+            (fact "snapshot does not see data written during snapshot"
+              snapshot-objects =not=> (contains (array-eq during-1))
+              snapshot-objects =not=> (contains (array-eq during-2)))
+
+            (fact "original pail does not contain data written before snapshot"
+              pail-objects =not=> (contains (array-eq before-1))
+              pail-objects =not=> (contains (array-eq before-2)))
+
+            (fact "original pail contains data written during snapshot"
+              pail-objects => (contains (array-eq during-1))
+              pail-objects => (contains (array-eq during-2)))))))))

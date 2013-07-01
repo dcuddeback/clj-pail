35906e4e1c10c76de17776a272dd60051dc31f0b
David Cuddeback <david.cuddeback@gmail.com>
2013-07-01 14:54:33 -0700

    adds functions for creating and opening pails

--- src/main/clojure/clj_pail/core.clj
+++ src/main/clojure/clj_pail/core.clj
@@ -1,8 +1,25 @@
 (ns clj-pail.core
-  (:import (com.backtype.hadoop.pail PailSpec PailStructure)))
+  (:import (com.backtype.hadoop.pail Pail PailSpec PailStructure)))
 
 
 (defn ^PailSpec spec
   "Builds a PailSpec from a PailStructure."
   [^PailStructure structure]
   (PailSpec. structure))
+
+
+(defn ^Pail pail
+  "Opens an existing Pail."
+  ([path]
+   (Pail. path))
+  ([fs path]
+   (Pail. fs path)))
+
+
+(defn ^Pail create
+  "Creates a Pail from a PailSpec at `path`."
+  [^PailSpec spec path & {:keys [filesystem fail-on-exists]
+                          :or {fail-on-exists true}}]
+  (if filesystem
+    (Pail/create filesystem path spec fail-on-exists)
+    (Pail/create path spec fail-on-exists)))

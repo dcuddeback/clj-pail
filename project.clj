50992de9b9823ac9be232b68a16db8ec6405245d
David Cuddeback <david.cuddeback@gmail.com>
2013-07-16 13:41:36 -0700

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pail "0.1.3-SNAPSHOT"
+(defproject clj-pail "0.1.3"
   :description "A Clojure abstraction for Pail"
   :url "https://github.com/dcuddeback/clj-pail"
   :license {:name "MIT License"

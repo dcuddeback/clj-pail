a1ebd1dbfedaca94855e0af8d5b80d2dfe4784fc
David Cuddeback <david.cuddeback@gmail.com>
2014-02-25 07:26:46 -0800

    lein-release plugin: bumped version from 0.1.5 to 0.1.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pail "0.1.5"
+(defproject clj-pail "0.1.6-SNAPSHOT"
   :description "A Clojure abstraction for Pail"
   :url "https://github.com/dcuddeback/clj-pail"
   :license {:name "MIT License"
